51926f1bb19b8f317e65c390a2928bc87d39511c
Md Morsalin Hossain <mdmorsalin80@gmail.com>
2024-09-30 23:10:17 +0600

    [Main] Adding view binding
    Signed-off-by: Md Morsalin Hossain <mdmorsalin80@gmail.com>

--- app/src/main/java/com/example/dailytakapoysha/MainActivity.kt
+++ app/src/main/java/com/example/dailytakapoysha/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.dailytakapoysha
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dailytakapoysha.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
